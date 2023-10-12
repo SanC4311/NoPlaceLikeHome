81088058bc6dab2720c3d67e6ef39e0bc7aeaece
Sanchit Singh <sanc4311@gmail.com>
2023-10-11 23:59:50 -0700

    Initialised Grid System constructor

---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem :
+{
+    private int width;
+    private int height;
+    // Start is called before the first frame update
+    public GridSystem(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
