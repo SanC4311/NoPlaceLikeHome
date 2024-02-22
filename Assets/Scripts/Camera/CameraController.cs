fa86f9bd9c231691348cd21489b8704dfb72eef9
Sanchit Singh <sanc4311@gmail.com>
2024-02-21 21:54:36 -0800

    Changed game camera + added another soldier for first playable test build

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,37 +6,11 @@ using Cinemachine;
 public class CameraController : MonoBehaviour
 {
 
-    private const float Y_ANGLE_MIN = 1.6f;
-    private const float Y_ANGLE_MAX = 7f;
-
-    private Vector3 offset;
-
-    private CinemachineTransposer cinemachineTransposer;
-
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
-
-    private void Start()
-    {
-        cinemachineTransposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
-        offset = cinemachineTransposer.m_FollowOffset;
-
-    }
     // Update is called once per frame
     void Update()
     {
-        camMovement();
         camRotation();
-        camZoom();
-    }
-
-    private void camMovement()
-    {
-        Vector2 inputMoveDirection = GameInput.Instance.GetCamMoveVector();
-
-        float camSpeed = 10f;
-
-        Vector3 movement = transform.forward * inputMoveDirection.y + transform.right * inputMoveDirection.x;
-        transform.position += movement * camSpeed * Time.deltaTime;
     }
 
     private void camRotation()
@@ -47,16 +21,5 @@ public class CameraController : MonoBehaviour
         float rotSpeed = 100f;
 
         transform.eulerAngles += rotVector * rotSpeed * Time.deltaTime;
-    }
-
-    private void camZoom()
-    {
-        float zoomIncAmount = 1f;
-        offset.y += GameInput.Instance.GetCamZoom() * zoomIncAmount;
-
-        offset.y = Mathf.Clamp(offset.y, Y_ANGLE_MIN, Y_ANGLE_MAX);
-
-        float zoomSmooth = 5f;
-        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, offset, Time.deltaTime * zoomSmooth);
     }
 }
